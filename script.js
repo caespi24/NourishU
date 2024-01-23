9d2d78792cc65c456da24701d09def9e12aecd60
caespi24 <christian.espiridion21@gmail.com>
2024-01-23 16:19:32 +0800

    Initial Commit

---
+++ script.js
@@ -0,0 +1,134 @@
+$('.username').on('click', function() {
+    $(this).addClass('active');
+    $('.password').removeClass('active');
+    $('.email').removeClass('active');
+    $('.fname').removeClass('active');
+    $('.lname').removeClass('active');
+    $('.age').removeClass('active');
+    $('.gender').removeClass('active');
+    $('.bdate').removeClass('active');
+    $('.weight').removeClass('active');
+    $('.height').removeClass('active');
+});
+
+$('.email').on('click', function() {
+    $(this).addClass('active');
+    $('.password').removeClass('active');
+    $('.username').removeClass('active');
+    $('.fname').removeClass('active');
+    $('.lname').removeClass('active');
+    $('.age').removeClass('active');
+    $('.gender').removeClass('active');
+    $('.bdate').removeClass('active');
+    $('.weight').removeClass('active');
+    $('.height').removeClass('active');
+});
+
+$('.password').on('click', function() {
+    $(this).addClass('active');
+    $('.username').removeClass('active');
+    $('.email').removeClass('active');
+    $('.fname').removeClass('active');
+    $('.lname').removeClass('active');
+    $('.age').removeClass('active');
+    $('.gender').removeClass('active');
+    $('.bdate').removeClass('active');
+    $('.weight').removeClass('active');
+    $('.height').removeClass('active');
+});
+
+$('.fname').on('click', function() {
+    $(this).addClass('active');
+    $('.password').removeClass('active');
+    $('.email').removeClass('active');
+    $('.username').removeClass('active');
+    $('.lname').removeClass('active');
+    $('.age').removeClass('active');
+    $('.gender').removeClass('active');
+    $('.bdate').removeClass('active');
+    $('.weight').removeClass('active');
+    $('.height').removeClass('active');
+});
+
+$('.lname').on('click', function() {
+    $(this).addClass('active');
+    $('.password').removeClass('active');
+    $('.email').removeClass('active');
+    $('.fname').removeClass('active');
+    $('.username').removeClass('active');
+    $('.age').removeClass('active');
+    $('.gender').removeClass('active');
+    $('.bdate').removeClass('active');
+    $('.weight').removeClass('active');
+    $('.height').removeClass('active');
+});
+
+$('.age').on('click', function() {
+    $(this).addClass('active');
+    $('.password').removeClass('active');
+    $('.email').removeClass('active');
+    $('.fname').removeClass('active');
+    $('.lname').removeClass('active');
+    $('.username').removeClass('active');
+    $('.gender').removeClass('active');
+    $('.bdate').removeClass('active');
+    $('.weight').removeClass('active');
+    $('.height').removeClass('active');
+});
+
+$('.gender').on('click', function() {
+    $(this).addClass('active');
+    $('.password').removeClass('active');
+    $('.email').removeClass('active');
+    $('.fname').removeClass('active');
+    $('.lname').removeClass('active');
+    $('.age').removeClass('active');
+    $('.username').removeClass('active');
+    $('.bdate').removeClass('active');
+    $('.weight').removeClass('active');
+    $('.height').removeClass('active');
+});
+
+$('.bdate').on('click', function() {
+    $(this).addClass('active');
+    $('.password').removeClass('active');
+    $('.email').removeClass('active');
+    $('.fname').removeClass('active');
+    $('.lname').removeClass('active');
+    $('.age').removeClass('active');
+    $('.gender').removeClass('active');
+    $('.username').removeClass('active');
+    $('.weight').removeClass('active');
+    $('.height').removeClass('active');
+});
+
+$('.weight').on('click', function() {
+    $(this).addClass('active');
+    $('.password').removeClass('active');
+    $('.email').removeClass('active');
+    $('.fname').removeClass('active');
+    $('.lname').removeClass('active');
+    $('.age').removeClass('active');
+    $('.gender').removeClass('active');
+    $('.bdate').removeClass('active');
+    $('.username').removeClass('active');
+    $('.height').removeClass('active');
+});
+
+$('.height').on('click', function() {
+    $(this).addClass('active');
+    $('.password').removeClass('active');
+    $('.email').removeClass('active');
+    $('.fname').removeClass('active');
+    $('.lname').removeClass('active');
+    $('.age').removeClass('active');
+    $('.gender').removeClass('active');
+    $('.bdate').removeClass('active');
+    $('.weight').removeClass('active');
+    $('.username').removeClass('active');
+});
+
+$(document).ready(function(){
+    //Modal init
+    $('.modal').modal();
+})
